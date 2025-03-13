590afeb5bd7660d61a68d03c4dafddd7ef90a41c
Sidd <sgupta.ctr@muellerwp.com>
2025-03-13 18:15:25 -0400

    Changed default logging level to CRITICAL. Bumped version.

--- src/chirpstack_fuota_client/utils/logging.py
+++ src/chirpstack_fuota_client/utils/logging.py
@@ -4,27 +4,35 @@ import sys
 LOG_FORMAT = "CHIRPSTACK-FUOTA-CLIENT - %(levelname)s: \t%(message)s"
 
 
-def setup_logging(level=logging.INFO, **kwargs):
+def setup_logging(level=logging.CRITICAL, **kwargs):
     """Configure logging for the chirpstack client.
 
     Args:
         level: Logging level (default: DEBUG)
     """
-    logging.basicConfig(level=level, format=LOG_FORMAT, stream=sys.stdout, **kwargs)
-
+    custom_logger = logging.getLogger("chirpstack_fuota_client")
+    custom_logger.setLevel(level)
+    
+    # Create console handler
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(level)
+    
+    # Create formatter and add it to the handlers
+    formatter = logging.Formatter(LOG_FORMAT)
+    ch.setFormatter(formatter)
+    
+    # Add the handlers to the logger
+    if not custom_logger.handlers:
+        custom_logger.addHandler(ch)
+    
     # Set log levels for noisy third-party libraries
     logging.getLogger("grpc").setLevel(logging.WARNING)
     logging.getLogger("google").setLevel(logging.WARNING)
     logging.getLogger("urllib3").setLevel(logging.WARNING)
 
-    # Create logger for chirpstack_fuota_client
-    logger = logging.getLogger("chirpstack_fuota_client")
-    logger.setLevel(logging.WARNING)
-    logger.info("Logging initialized with level %s", logging.getLevelName(level))
-    logger.setLevel(level)
+    custom_logger.info("Logging initialized with level %s", logging.getLevelName(level))
 
-    return logger
-
+    return custom_logger
 
 # Create default logger instance
 logger = setup_logging()
